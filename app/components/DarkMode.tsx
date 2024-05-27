fd9297a748e4017f37519e2c42786e5ea532ecdf
keneogu <kc101com@gmail.com>
2024-05-27 14:28:08 +0100

    add dark mode component

---
+++ app/components/DarkMode.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function DarkMode() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  
+  return (
+    <div>
+      {mounted && (currentTheme === "dark" ? (
+        <MdLightMode
+          className="text-xl cursor-pointer"
+          onClick={() => setTheme("light")}
+        />
+      ) : (
+        <BsFillMoonFill
+          className="text-xl cursor-pointer"
+          onClick={() => setTheme("dark")}
+        />
+      ))}
+    </div>
+  );
+}
