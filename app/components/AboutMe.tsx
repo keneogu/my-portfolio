1d24dde9e4192c2f24ac5b7c2d5bdc7759def384
keneogu <kc101com@gmail.com>
2024-06-05 13:56:09 +0100

    update the height and width of the about page img

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -6,17 +6,19 @@ const AboutMe = () => {
   return (
     <div className="w-full dark:bg-[#0b0a0a]">
       <Container>
-        <div>
-          <h3>About Me</h3>
+        <div className="w-full p-6">
+        <h2 className="text-center text-4xl font-bold mt-4 mb-8 md:mb-12">
+            About Me!
+          </h2>
           <div className="flex flex-col lg:grid grid-cols-2 justify-between text-center my-9 py-6">
             <div className="text-center flex items-center justify-center bg-light-bg dark:bg-dark-bg bg-cover bg-no-repeat bg-center py-11">
               <div className="rounded-full relative w-[200px] h-[200px] lg:w-[400px] lg:h-[400px] justify-self-center">
                 <Image
-                  src="/images/hero-image.png"
+                  src="/images/projects/profile.png"
                   alt="hero image"
                   className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  width={300}
-                  height={300}
+                  width={900}
+                  height={900}
                 />
               </div>
             </div>
