688812980502faf69f53f728db180898578acc7b
keneogu <kc101com@gmail.com>
2024-06-05 14:02:43 +0100

    add the updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact/Contact";
+import Education from "./components/Education";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Languages from "./components/Language/Languages";
+import MySkills from "./components/MySkills";
+import Project from "./components/Projects/Project";
+// import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex justify-center align-middle">
-      <h1>Started Portfolio</h1>
+    <main>
+        <Header />
+        <Languages />
+        <AboutMe />
+        <MySkills />
+        <Education />
+        <Project />
+        <Contact />
+        <Footer />
     </main>
-  )
+  );
 }
