73bf01fbd3d2b75c64423689b9b1469e240c5335
keneogu <kc101com@gmail.com>
2024-06-05 14:00:06 +0100

    add project data

---
+++ app/components/Projects/projectData.ts
@@ -0,0 +1,43 @@
+const projectData = [
+  {
+    id: 1,
+    title: "Nextjs Portfolio Website",
+    description:
+      "This is my Portfolio been built using Nextjs. The portfolio also showcases my updated level of expertise and my different projects",
+    image: "/images/projects/portfolio_img.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/keneogu/",
+    previewUrl: "https://kene-portfolio.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "React Portfolio Website",
+    description:
+      "This is portfolio built with react that showcases the projects built on my software engineering journey",
+    image: "/images/projects/portfolio.png",
+    tag: ["All", "Web"],
+    gitUrl: "github.com/keneogu/React-Portfolio",
+    previewUrl: "https://kene-portfolio.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Crypto Website",
+    description:
+      "This app is made with React and Redux, and its purpose is to fetch data for various cryptocurrencies from an external api and you can also filter data to get a specific coin",
+    image: "/images/projects/crypto.png",
+    tag: ["All", "Web"],
+    gitUrl: "github.com/keneogu/crypto-catalogue",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Tweet Social Media",
+    description: "A ruby on rails social media app where users can sign up, follow one another, and post tweets.",
+    image: "/images/projects/twitter-redesign.png",
+    tag: ["All", "Web"],
+    gitUrl: "github.com/keneogu/twitter-redesign",
+    previewUrl: "/",
+  },
+];
+
+export default projectData;
