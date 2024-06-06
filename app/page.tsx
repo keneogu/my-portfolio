efe1f90ec3b62a224c6add4d3381a5aeb5c68b22
keneogu <kc101com@gmail.com>
2024-06-07 00:51:19 +0100

    remove the commented out code

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,18 @@ import Header from "./components/Header";
 import Languages from "./components/Language/Languages";
 import MySkills from "./components/MySkills";
 import Project from "./components/Projects/Project";
-// import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
-        <Header />
-        <Languages />
-        <AboutMe />
-        <MySkills />
-        <Education />
-        <Project />
-        <Contact />
-        <Footer />
+      <Header />
+      <Languages />
+      <AboutMe />
+      <MySkills />
+      <Education />
+      <Project />
+      <Contact />
+      <Footer />
     </main>
   );
 }
